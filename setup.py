8aa11f4a65ccb9650566c0fe33c1ca179ecad2c9
Howell McCullough <hdmccullough@gmail.com>
2018-04-24 15:26:25 -0400

    Update project for 0.1.0.dev1 release

--- setup.py
+++ setup.py
@@ -1,30 +1,57 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+root = path.abspath(path.dirname(__file__))
+
+with open(path.join(root, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     # Application Name:
     name='alchemist_stack',
 
     # Version Number:
-    version='0.1.0',
+    version='0.1.0.dev1',
+
+    # Classifiers
+    classifiers = [
+        'Development Status :: 1 - Development',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Databases',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
+    ],
+
+    # Keywords
+    keywords='sqlalchemy databases',
 
     # Application Author Details:
     author='H.D. "Chip" McCullough IV',
     author_email='hdmccullough.work@gmail.com',
 
     # Packages:
-    packages=['alchemist_stack'],
-
-    # Include Additional Files into the Package:
-    include_package_date=True,
+    packages=find_packages(exclude=['models', 'repos', 'tables', 'tests']),
 
     # Details:
     url='https://github.com/mcculloh213/alchemist-stack',
-    license='LICENSE.md',
+    project_urls={
+        'Documentation': 'https://github.com/mcculloh213/alchemist-stack',
+        #'Funding': 'https://github.com/mcculloh213/alchemist-stack',
+        'Source': 'https://github.com/mcculloh213/alchemist-stack',
+        'Issue Tracker': 'https://github.com/mcculloh213/alchemist-stack/issues'
+    },
+    license='MIT',
     description='A Thread-Safe, Multi-Session/Multi-Connection Model-Repository-Context base for SQL Alchemy',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
     # Dependent Packages (Distributions):
     install_requires=[
         'sqlalchemy',
     ],
+
+    # Requires Python Version:
+    python_requires='>=3'
 )
